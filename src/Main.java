f5b81ec4c94f795ef3696ce3cc133a718ffb6447
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-09-08 19:38:20 -0400

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -14,20 +14,32 @@ public class Main {
         int switchValue = 3;
 
         switch (switchValue) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Was a 3, a 4, or a 5");
                 System.out.println("Actually it was a " + switchValue);
-                break;
-            default:
-                System.out.println("Value was neither 1, 2, 3, 4 , or 5");
-                break;
-
+            }
+            default -> System.out.println("Value was neither 1, 2, 3, 4 , or 5");
         }
+
+        String month = "cx";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+
     }
+
+    public static String getQuarter(String month) {
+
+        return switch (month) {
+            case "January", "February", "March" -> { yield "1st"; }
+            case "April", "May", "June" -> "2nd";
+            case "July", "August", "September" -> "3rd";
+            case "October", "Novemeber", "December" -> "4th";
+            default -> {
+                String badResponse = month + " is bad";
+                yield badResponse;
+            }
+        };
+    }
+
 }
